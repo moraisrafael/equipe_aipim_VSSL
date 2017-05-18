fcbf7869ca6d96995a46fb60e889c7d221ea1b28
Rafael Morais da Cunha <rafaelmoraisdacunha@gmail.com>
2017-05-18 14:25:23 -0300

    Update base.ino

--- base.ino
+++ base.ino
@@ -17,7 +17,8 @@ RF24Network network(radio);      // Network uses that radio
 
 void setup(void)
 {
-  Serial.begin(57600);
+  Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("iniciando execucao da base");
  
   SPI.begin();
@@ -33,89 +34,118 @@ void setup(void)
 }
 void loop(void){
   network.update();                  // Check the network regularly
+
+  Serial.println("loop");
+  while (Serial.available() == 0);
+  int test = Serial.parseInt();
+  Serial.println(test);
+  switch (test) {
+    case 1 :// testa escrita digital
+      {
+      for (int i = 0; i < 2; i++) {
+        escreve_pino_rede(01, pino_led, HIGH);
+        delay(500);
+        escreve_pino_rede(01, pino_led, LOW);
+        delay(500);
+      }
+      break;
+      }
+    case 2 : // testa leitura digital
+      {
+      boolean val = le_pino_rede(01, pino_potenciometro);
+      if (val == HIGH)
+        Serial.println("HIGH");
+      else
+        Serial.println("LOW");
+      break;
+      }
+    case 3 : // testa escrita pwm
+      {
+      int brightness = 0;
+      for (int i = 0; i < 1024; i++) {
+        escreve_pwm_pino_rede(01, pino_led, brightness);
+        brightness = (brightness + 5)%256;
+        delay(30);
+      }
+      break;
+      }
+    case 4 : // testa leitura analogica
+      {
+      unsigned long time = millis();
+      while (millis() - time < 5000) {
+        Serial.println(le_pino_analogico_rede(01, pino_potenciometro));
+      }
+      break;
+      }
+    case 65 : // testa perda de pacotes
+      {
+      int count = 0;
+      unsigned long latencia, latencia_media = 0;
+      Serial.println("iniciando teste de perda de pacotes");
+      for (int i = 0; i < 1000; i++) {
+        Serial.print(i);
+        latencia = ping(01);
+        if (latencia == 0)
+          count++;
+        latencia_media += latencia;
+      }
+      latencia_media = latencia_media/(1000 - count);
+      Serial.print("pacotes perdidos: ");
+      Serial.println(count);
+      Serial.print("latencia media: ");
+      Serial.println(latencia_media);
+      break;
+      }
+    case -1 : // teste leitura analogica e escrita pwm
+      {
+      unsigned long time = millis();
+      while (millis() - time < 5000) {
+        int valor = le_pino_analogico_rede(01, pino_potenciometro);
+        escreve_pwm_pino_rede(01, pino_led, valor);
+      }
+      break;
+      }
+    case -2 :  // teste 65, com 3 radios ao mesmo tempo
+      {
+      int count1, count2, count3;
+      unsigned long latencia, latencia_media1, latencia_media2, latencia_media3;
+      count1 = count2 = count3 = 0;
+      latencia_media1 = latencia_media2 = latencia_media3 = 0;
+      for (int i = 0; i < 1000; i++) {
+        latencia = ping(01);
+        if (latencia == 0)
+          count1++;
+        latencia_media1 += latencia;
   
-  int test = Serial.read();
-  if (test == 1) { // testa escrita digital
-    for (int i = 0; i < 2; i++) {
-      escreve_pino_rede(01, pino_led, HIGH);
-      delay(500);
-      escreve_pino_rede(01, pino_led, LOW);
-      delay(500);
-    }
-  } else if (test == 2) { // testa leitura digital
-    boolean val = le_pino_rede(01, pino_potenciometro);
-    if (val == HIGH)
-      Serial.println("HIGH");
-    else
-      Serial.println("LOW");
-  } else if (test == 3) { // testa escrita pwm
-    int brightness = 0;
-    for (int i = 0; i < 1024; i++) {
-      escreve_pwm_pino_rede(01, pino_led, brightness);
-      brightness = (brightness + 5)%256;
-      delay(30);
-    }
-  } else if (test == 4) { // testa leitura analogica
-    unsigned long time = millis();
-    while (millis() - time < 5000) {
-      Serial.println(le_pino_analogico_rede(01, pino_potenciometro));
-    }
-  } else if (test == 65) { // testa perda de pacotes
-    int count = 0;
-    unsigned long latencia, latencia_media = 0; 
-    for (int i = 0; i < 1000; i++) {
-      latencia = ping(01);
-      if (latencia == 0)
-        count++;
-      latencia_media += latencia;
-    }
-    latencia_media = latencia_media/(1000 - count);
-    Serial.print("pacotes perdidos: ");
-    Serial.println(count);
-    Serial.print("latencia media: ");
-    Serial.println(latencia_media);
-  } else if (test == -1) { // teste leitura analogica e escrita pwm
-    unsigned long time = millis();
-    while (millis() - time < 5000) {
-      int valor = le_pino_analogico_rede(01, pino_potenciometro);
-      escreve_pwm_pino_rede(01, pino_led, valor);
-    }
-  } else if (test == -2) {  // teste 65, com 3 radios ao mesmo tempo
-    int count1, count2, count3;
-    unsigned long latencia, latencia_media1, latencia_media2, latencia_media3;
-    count1 = count2 = count3 = 0;
-    latencia_media1 = latencia_media2 = latencia_media3 = 0;
-    for (int i = 0; i < 1000; i++) {
-      latencia = ping(01);
-      if (latencia == 0)
-        count1++;
-      latencia_media1 += latencia;
-
-      latencia = ping(02);
-      if (latencia == 0)
-        count2++;
-      latencia_media2 += latencia;
-
-      latencia = ping(03);
-      if (latencia == 0)
-        count3++;
-      latencia_media3 += latencia;
-    }
-    latencia_media1 = latencia_media1/(1000 - count1);
-    Serial.print("pacotes perdidos: ");
-    Serial.println(count1);
-    Serial.print("latencia media: ");
-    Serial.println(latencia_media1);
-    latencia_media2 = latencia_media2/(1000 - count2);
-    Serial.print("pacotes perdidos: ");
-    Serial.println(count2);
-    Serial.print("latencia media: ");
-    Serial.println(latencia_media2);
-    latencia_media3 = latencia_media3/(1000 - count3);
-    Serial.print("pacotes perdidos: ");
-    Serial.println(count3);
-    Serial.print("latencia media: ");
-    Serial.println(latencia_media3);
+        latencia = ping(02);
+        if (latencia == 0)
+          count2++;
+        latencia_media2 += latencia;
+  
+        latencia = ping(03);
+        if (latencia == 0)
+          count3++;
+        latencia_media3 += latencia;
+      }
+      latencia_media1 = latencia_media1/(1000 - count1);
+      Serial.print("pacotes perdidos: ");
+      Serial.println(count1);
+      Serial.print("latencia media: ");
+      Serial.println(latencia_media1);
+      latencia_media2 = latencia_media2/(1000 - count2);
+      Serial.print("pacotes perdidos: ");
+      Serial.println(count2);
+      Serial.print("latencia media: ");
+      Serial.println(latencia_media2);
+      latencia_media3 = latencia_media3/(1000 - count3);
+      Serial.print("pacotes perdidos: ");
+      Serial.println(count3);
+      Serial.print("latencia media: ");
+      Serial.println(latencia_media3);
+      break;
+      }
+    default:
+      Serial.println("comando invalido");
   }
 }
 
@@ -191,11 +221,14 @@ unsigned long ping(uint16_t endereco) {
   RF24NetworkHeader header(endereco, 65);
   unsigned long time;
   bool ack;
-
+  Serial.print(" ping ");
   time = micros();
   ack = network.write(header,NULL,0);
-  if (ack == true)
+  if (ack == true) {
+    Serial.println(micros() - time);
     return micros() - time;
-  else
+  } else {
+    Serial.println("falhou");
     return 0;
+  }
 }
