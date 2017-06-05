e5c3138f54b00c41a604466437e71f12caa52a96
Rafael Morais da Cunha <rafaelmoraisdacunha@gmail.com>
2017-06-04 23:19:20 -0300

    atualizacao do codigo do robo

---
+++ arduino/readme.md
@@ -0,0 +1,10 @@
+Aqui estão os codigos fonte usados no arduino para controle do rádio e dos robôs.
+
+robo        código a ser usado pelos robôs
+base        código para comunicação do computador com os robôs via rádio
+echo        código teste do rádio
+echo\_base  código teste do rádio
+
+Para uso do rádio nRF24l01, estamos usando as bibliotecas:
+<a href =https://github.com/nRF24/RF24>RF24</a> (14/maio/2017)
+<a href=https://github.com/nRF24/RF24Network>RF24Network</a> (15/abril/2017)
--- arduino/robo/robo.ino
+++ arduino/robo/robo.ino
@@ -1,100 +1,107 @@
 #include <RF24Network.h>
 #include <RF24.h>
 #include <SPI.h>
+#include "protocoloRobo.h"
 
-#define MAX_PAYLOAD_SIZE2 10
-#define pino_led 8
-#define pino_potenciometro A0
 
-typedef struct payload_escreve_pino {
-  int pino;
-  boolean valor;
-} payload_escreve_pino;
+RF24 radio(9, 10); // radio ligado nos pinos 9 e 10
+RF24Network network(radio);
 
-typedef struct payload_escreve_pino_pwm {
-  int pino;
-  int valor;
-} payload_escreve_pino_pwm;
+void verificaRadio();
+void trataMensagens();
+void movimentaRobo();
 
-void trata_msg();
-void escreve_pino(byte* payload);
-void le_pino(byte* payload);
-void escreve_pino_pwm(byte* payload);
-void le_pino_analogico(byte* payload);
+uint16_t enderecoRobo = 01;
+uint16_t enderecoBase = 00;
 
-RF24 radio(9,10);                // nRF24L01(+) radio attached using Getting Started board 
-RF24Network network(radio);      // Network uses that radio
+RadioBuffer radioReadBuffer;
 
-void setup(void)
-{
+void setup(void) {
   Serial.begin(57600);
-  Serial.println("iniciando execucao da base");
- 
+  Serial.println("Executando Robo");
+
   SPI.begin();
   radio.begin();
-
-  radio.setPALevel(RF24_PA_MAX);          // Potência da transmissão em 0dB ( 1mW )
-  radio.setDataRate(RF24_250KBPS);        // O padrão é 1Mbps
-  radio.setCRCLength(RF24_CRC_16);        // Comprimento do CRC: 8 ou 16 bits
-  radio.enableDynamicPayloads();          // Habilita mensagens de tamanho dinâmico
-  radio.maskIRQ(1,1,0);                   // Interrupção apenas para mensagens recebidas
-
-  network.begin(/*channel*/ 90, /*node address*/ 01);
-
-  // Habilita interrupção no pino 2 do Arduino
-  attachInterrupt(digitalPinToInterrupt(2), trata_msg, FALLING);
-
-  pinMode(pino_led, OUTPUT);
-  pinMode(pino_potenciometro, INPUT);
-}
-void loop(void){
-  network.update();                  // Check the network regularly  
-  // mensagens recebidas sao tratadas com interrupcoes
+  network.begin(/*channel*/ 90, /*node address*/ enderecoRobo);
 }
 
-void trata_msg() {
-  RF24NetworkHeader header;
-  byte payload[MAX_PAYLOAD_SIZE2];
+void loop() {
+  verificaRadio();
+  trataMensagens();
+  movimentaRobo();
+}
 
-  while (network.available()) {
-    network.read(header, payload, MAX_PAYLOAD_SIZE2);
-    
-    if (header.type == 1)
-      escreve_pino(payload);
-    else if (header.type == 2)
-      le_pino(payload);
-    else if (header.type == 3)
-      escreve_pino_pwm(payload);
-    else if (header.type == 4)
-      le_pino_analogico(payload);
-    else
-      Serial.println("tipo nao reconhecido");
+void verifica_radio() {
+  static RF24NetworkHeader header;
+  static byte msg[TAM_MAX_MSG];
+  network.update(); // necessario para network funcionar corretamente
+  while (network.available())
+  {
+    if (radioReadBuffer.isFull())
+      return;
+    network.read(header, msg, TAM_MAX_MSG);
+    radioReadBuffer.write(header, msg);
+    network.update();
   }
 }
 
-void escreve_pino(byte* payload) {
-  digitalWrite(((payload_escreve_pino*) payload)->pino, ((payload_escreve_pino*) payload)->valor);
+//typedef int payloadEscrevePino;
+
+void trata_mensagens() {
+  static uint8_t tipo;
+  byte msg[TAM_MAX_MSG];
+  static RF24NetworkHeader header(enderecoBase, 0);
+
+  while (radioReadBuffer.available()) {
+    radioReadBuffer.read(tipo, msg);
+    switch (tipo) {
+      case EscrevePino:
+        digitalWrite(((payloadEscrevePino*)msg)->pino, ((payloadEscrevePino*)msg)->estado);
+        break;
+      case EscrevePwm:
+        analogWrite(((payloadEscrevePwm*)msg)->pino, ((payloadEscrevePwm*)msg)->pwm);
+        break;
+      case MotorDirecaoPwm:
+        break;
+      case MotorDirecaoVelocidade:
+        break;
+      case MotorDirecaoDistancia:
+        break;
+      case EscreveConfiguracao:
+        break;
+      case Ping:
+        // ACK automatico
+        break;
+      case Echo:
+        header.type = Echo;
+        network.write(header, msg, TAM_MAX_MSG);
+        break;
+      case softwareReset:
+        // reset();
+        break;
+      case LeituraPino:
+        header.type = LeituraPino;
+        ((payloadLeituraPino*)msg)->estado = digitalRead(((payloadLeituraPino*)msg)->pino);
+        network.write(header, msg, sizeof(payloadLeituraPino));
+        break;
+      case LeituraAnalogica:
+        header.type = LeituraPino;
+        ((payloadLeituraAnalogica*)msg)->estado = analogRead(((payloadLeituraAnalogica*)msg)->pino);
+        network.write(header, msg, sizeof(payloadLeituraPino));
+        break;
+      case LeituraVelocidade:
+        break;
+      case LeituraEncoder:
+        break;
+      case LeituraConfiguracao:
+        break;
+      default:
+        // mensgaem nao reconhecida
+        break;
+    }
+  }
 }
 
-void le_pino(byte* payload) {
-  RF24NetworkHeader header(00, 2);
-  boolean leitura;
+void movimentaRobo() {
 
-  leitura = digitalRead((int)*payload);
-
-  network.write(header,&leitura,sizeof(boolean));
-}
-
-void escreve_pino_pwm(byte* payload) {
-
-  analogWrite(((payload_escreve_pino_pwm*)payload)->pino, ((payload_escreve_pino_pwm*)payload)->valor);
-}
-
-void le_pino_analogico(byte* payload) {
-  RF24NetworkHeader header(00, 4);
-  int leitura;
-
-  leitura = analogRead((int)*payload);
-
-  network.write(header,&leitura,sizeof(int));
 }
