e15644245404e8206cdbf3198468a933202a8827
Victor Barroso <vdbarroso@gmail.com>
2017-06-22 17:24:13 -0300

    tracking now working
    Signed-off-by: Victor Barroso <vdbarroso@gmail.com>

--- python/readme.md
+++ python/readme.md
@@ -1,1 +1,3 @@
 arduinoserial.py - responsavel pela comunicacao do pc com o arduino via serial. baseado no <a href="https://github.com/wiseman/arduino-serial">arduinoserial.py</a> (08/fevereiro/2016)
+
+image.py - responsável pelo reconhecimento do campo e dos robos
--- python/tracking.py
+++ python/tracking.py
@@ -1,8 +1,3 @@
-"""This script detects the presence of blobs and tracks it as it moves 
-around in the video frames.
-
-Reference code: http://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
-"""
 
 import sys
 from collections import deque
@@ -13,42 +8,32 @@ import imutils
 
 BUFFER_SIZE = 64
 
-# pixel values set as reference for thresholding
+# bgr pixel values set as reference for thresholding
 color = {
     'yellow': [76, 208, 201],
     'blue': [198, 129, 56],
-    'red': [74, 62, 192],
-}
+    'red': [74, 62, 192]}
 
-
-# Hue range is [0..179], Saturation is [0..255] and Value is [0..255]
-# here are some default values.
-hsv_color = {
-    'yellow': 60,
-    'blue': 233,
-    'red': 0,
-}
-
-
-def get_boundaries_from_HSV_range(color, range):
+def get_HSV_boundaries_from_RGB(color_bgr, value_range):
     """Function to provide some default values for testing. Beware that
     HSV values for white and black colors goes through all the hue range
     without any change to saturation and value.
     """
-
-    lower = np.array([color, 33, 33], dtype='int16')
-    upper = np.array[(color, 255, 255], dtype='int16')
+    color_bgr = np.uint8([[color_bgr]])
+    hsv_color = cv2.cvtColor(color_bgr,cv2.COLOR_BGR2HSV)[0][0][0]
+    
+    lower = np.array([hsv_color-value_range, 125, 15], dtype='int16')
+    upper = np.array([hsv_color+value_range, 255, 230], dtype='int16')
 
     lower[0] = np.clip(lower[0], 0, 179)
     upper[0] = np.clip(upper[0], 0, 179)
 
     return (lower, upper)
 
-
-def get_boundaries_from_BRG_range(color, upper, lower):
+def get_boundaries_from_BRG_range(color_bgr, upper, lower):
     # calculate upper and lower boundaries in tuple
-    lower = np.array(color, dtype='int16') - lower
-    upper = np.array(color, dtype='int16') + upper
+    lower = np.array(color_bgr, dtype='int16') - lower
+    upper = np.array(color_bgr, dtype='int16') + upper
     
     # negative values and higher than 255 are not valid
     lower = np.clip(lower, 0, 255)
@@ -56,28 +41,38 @@ def get_boundaries_from_BRG_range(color, upper, lower):
 
     return (lower, upper)
 
-
-def main():
+def main(videoSource):
     pts = deque(maxlen=BUFFER_SIZE)
-    camera = cv2.VideoCapture(sys.argv[1])
+    camera = cv2.VideoCapture(videoSource)
 
     while True:
         (grabbed, frame) = camera.read()
 
         if not grabbed:
+            print('Ended video')
             break
         
         # resize frame, and convert it to HSV
         frame = imutils.resize(frame, width=600)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        lower, upper = get_boundaries_from_HSV_range(hsv_color, 20)
+        lower, upper = get_HSV_boundaries_from_RGB(color['blue'], 40)
 
         mask = cv2.inRange(hsv, lower, upper)
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
 
-        cv2.imshow("Frame", mask)
+        # find contours in the mask
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        # initializes set of xy axes for the tracked items
+        center_list = []
+
+        # check if there is a contour
+        if len(cnts) > 0:
+            center_list = draw_contours(cnts, frame, pts)
+
+        cv2.imshow("Frame", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -85,8 +80,36 @@ def main():
     camera.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    if (len(sys.argv) != 2):
-        print('Programa requer um video como argumento. Saindo.')
-        sys.exit(1)
-    main()
+
+def draw_contours(cnts, frame, pts):
+
+    center_list = []
+    
+    # gets the biggest 5 biggest contours by area to track
+    biggest_cnts = sorted(cnts, key=cv2.contourArea)[:4]
+
+    # get the biggest contour in the mask
+    #c = max(cnts, key=cv2.contourArea)
+
+    for c in biggest_cnts:
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+
+        # tries to find the centroid using moments
+        M = cv2.moments(c)
+        new_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        center_list.append(new_center)
+
+        if radius > 10:
+            # draw circle and centroid on the frame
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255,255), 2)
+            cv2.circle(frame, new_center, 5, (0,0,255), -1)
+        
+        pts.appendleft(new_center)
+
+    return center_list
+
+
+main(sys.argv[1])
+
+
+
